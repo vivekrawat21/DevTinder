8a6cc715edadf55c80c4d9b16721dee77e7161a1
vivekrawat21 <vivekrwt2111@gmail.com>
2025-05-16 17:01:51 +0530

    fix: update CORS options to include PATCH method and improve token validation in user authentication middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ const app = express();
 const corsOptions = {
   origin: process.env.FRONTEND_URL,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
 };
 
 
